58a3cd44e525d3bebc92fc0526a663ca6776cfd7
Jatin <84621253+h0x0er@users.noreply.github.com>
2025-07-28 12:25:08 +0530

    configure repo

--- scripts/configure-git.sh
+++ scripts/configure-git.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-
+repo="$GITHUB_REPOSITORY"
 
 ## Configure git
 git config --global user.name "obj-builder-ci"
@@ -10,5 +10,5 @@ git config --global user.email "obj-builder-ci@users.noreply.github.com"
 
 # Configure origin
 git remote -v
-git remote set-url origin https://github.com/h0x0er/ecapture-objs.git
+git remote set-url origin "https://github.com/$repo.git"
 git remote -v
